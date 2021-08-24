e4cdb1f02c7bd043405a725fc1e60054128ad2c2
Andrei Khegai <hegai.andrei@gmail.com>
2021-08-24 16:55:07 +0600

    Initial Commit

---
+++ brewtimer/brewtimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  brewtimerApp.swift
+//  brewtimer
+//
+//  Created by Andrei Khegai on 24/8/21.
+//
+
+import SwiftUI
+
+@main
+struct brewtimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
